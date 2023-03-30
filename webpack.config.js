a6929e0aa9dd5ee3f3a44901dac4cff31c9bb000
Samantha Brunel <59365424+sabrunel@users.noreply.github.com>
2023-03-30 21:23:58 +0200

    Setup project and fetch data from the API

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+/* eslint-disable no-undef */
+module.exports = {
+    mode: 'development',
+    entry: {
+      main: path.resolve(__dirname, 'src', 'index.js'),
+    },
+    output: {
+        filename: '[name]-[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'source-map',
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      port: 3000,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: [
+              'style-loader',
+              'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        title: "Weather App",
+        filename: "index.html",
+        template: path.resolve(__dirname, 'src', 'index.html'),
+      })
+
+    ]
+
+}
